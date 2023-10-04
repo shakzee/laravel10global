d6e152076c3b53cca59869650aa7866b3551e6ae
shakzeeofficial <shakzee171@gmail.com>
2023-10-04 16:37:54 +0500

    added the vite in

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,13 +1,2 @@
-import $ from 'jquery';
-
-import.meta.glob([
-    '../theme/img/**'
-]);
-import.meta.glob([
-    '../theme/fonts/**'
-]);
-
-// $(function(){
-//     alert('working...');
-// })
-//import '../theme/js/script.js';
+//import './bootstrap';
+import '../them/js/custom.js'
